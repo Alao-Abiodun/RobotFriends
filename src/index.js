c861133a95ac05f89c1e0a6486f63b578f6b63d6
Alao-Abiodun <abiodunDev@gmail.com>
2020-04-02 09:00:50 +0100

    adding react-redux and redux library to the project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons'
+
+const logger = createLogger();
+const rootReducers = combineReducers({
+    searchRobots, requestRobots
+})
+const store = 
+                    createStore( rootReducers, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={ store }>
+        <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
